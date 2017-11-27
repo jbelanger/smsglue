c80109534620c2f195b6616ee09c4dff821eef60
joel.belanger <joel.belanger@N31002062>
2017-11-27 15:39:00 -0500

    .

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // Create cache directories
-//var fs = require('fs');
-//var path = require('path');
+var fs = require('fs');
+var path = require('path');
 
-//fs.existsSync = require('path').existsSync;
+var cache      = path.resolve(process.env.CACHE || 'cache'),
+    key        = path.resolve(process.env.CACHE || 'cache', 'key'),
+    devices    = path.resolve(process.env.CACHE || 'cache', 'devices'),
+    messages   = path.resolve(process.env.CACHE || 'cache', 'messages'),
+    provisions = path.resolve(process.env.CACHE || 'cache', 'provisions');
 
-//var cache      = path.resolve(process.env.CACHE || 'cache'),
-//    key        = path.resolve(process.env.CACHE || 'cache', 'key'),
-//    devices    = path.resolve(process.env.CACHE || 'cache', 'devices'),
-//    messages   = path.resolve(process.env.CACHE || 'cache', 'messages'),
-//    provisions = path.resolve(process.env.CACHE || 'cache', 'provisions');
-
-//if (!fs.existsSync(cache))      fs.mkdirSync(cache);
-//if (!fs.existsSync(key))        fs.mkdirSync(key);
-//if (!fs.existsSync(devices))    fs.mkdirSync(devices);
-//if (!fs.existsSync(messages))   fs.mkdirSync(messages);
-//if (!fs.existsSync(provisions)) fs.mkdirSync(provisions);
+if (!fs.existsSync(cache))      fs.mkdirSync(cache);
+if (!fs.existsSync(key))        fs.mkdirSync(key);
+if (!fs.existsSync(devices))    fs.mkdirSync(devices);
+if (!fs.existsSync(messages))   fs.mkdirSync(messages);
+if (!fs.existsSync(provisions)) fs.mkdirSync(provisions);
 
 module.exports = require('./app');
